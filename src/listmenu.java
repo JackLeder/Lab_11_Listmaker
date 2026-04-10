08497bbcd46ac62f97f3f34803100f9135caca4e
Jack Leder <lederjk@mail.uc.edu>
2026-04-10 10:21:17 -0400

    commit

--- src/listmenu.java
+++ src/listmenu.java
@@ -1,5 +1,4 @@
-
-import  java.util.Scanner;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class listmenu {
@@ -9,47 +8,87 @@ public class listmenu {
         ArrayList<String> list = new ArrayList<>();
 
         do {
-            String menu = safeinput.getRegExString(pipe, "Enter a command to put into the list [A = add] [D = delete] [I = insert] [P = print] [Q = quit] ", "[AaDdIiPpQq]");
+            String menu = safeinput.getRegExString(pipe,
+                    "Enter a command to put into the list [A = add] [D = delete] [I = insert] [P = print] [Q = quit] ",
+                    "[AaDdIiPpQq]");
+
             switch (menu.toUpperCase()) {
                 case "A":
-                    String add = safeinput.getNonZeroLenString(pipe, "Enter a string to add to the list");
-                    list.add(add);
+                    addItem(pipe, list);
                     break;
                 case "D":
-                    System.out.println("List is: " + list);
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println(i + ": " + list.get(i));
-                    }
-
-                    int delete = safeinput.getRangedInt(pipe, "Enter the index of the string to delete", 0, list.size() - 1);
-                    list.remove(delete);
+                    deleteItem(pipe, list);
                     break;
                 case "I":
-                    System.out.println("List is: " + list);
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println(i + ": " + list.get(i));
-                    }
-
-                    int insertIndex = safeinput.getRangedInt(pipe, "Enter the index of the string to insert", 0, list.size() - 1);
-                    String insertString = safeinput.getNonZeroLenString(pipe, "Enter a string to insert into the list");
-                    list.add(insertIndex, insertString);
+                    insertItem(pipe, list);
                     break;
                 case "P":
-                    //System.out.println("List is: " + list);
+                    printList(list);
                     break;
-
                 case "Q":
-                    boolean confirm = safeinput.getYNConfirm(pipe, "Are you sure you want to quit?");
-                    if (confirm) {
-                        done = true;
-
-                    }
-
+                    done = quitProgram(pipe);
                     break;
             }
+
             System.out.println("List is: " + list);
 
+        } while (!done);
+    }
 
-        }while(!done);
+
+    public static void addItem(Scanner pipe, ArrayList<String> list) {
+        String add = safeinput.getNonZeroLenString(pipe, "Enter a string to add to the list");
+        list.add(add);
+    }
+
+
+    public static void deleteItem(Scanner pipe, ArrayList<String> list) {
+        if (list.size() == 0) {
+            System.out.println("List is empty. Nothing to delete.");
+            return;
+        }
+
+        displayIndexedList(list);
+        int delete = safeinput.getRangedInt(pipe,
+                "Enter the index of the string to delete",
+                0, list.size() - 1);
+        list.remove(delete);
+    }
+
+
+    public static void insertItem(Scanner pipe, ArrayList<String> list) {
+        if (list.size() == 0) {
+            System.out.println("List is empty. Inserting at index 0.");
+            String insertString = safeinput.getNonZeroLenString(pipe, "Enter a string to insert");
+            list.add(insertString);
+            return;
+        }
+
+        displayIndexedList(list);
+        int insertIndex = safeinput.getRangedInt(pipe,
+                "Enter the index of the string to insert",
+                0, list.size() - 1);
+
+        String insertString = safeinput.getNonZeroLenString(pipe, "Enter a string to insert into the list");
+        list.add(insertIndex, insertString);
+    }
+
+
+    public static void printList(ArrayList<String> list) {
+        System.out.println("List is: " + list);
+    }
+
+
+    public static boolean quitProgram(Scanner pipe) {
+        boolean confirm = safeinput.getYNConfirm(pipe, "Are you sure you want to quit?");
+        return confirm;
+    }
+
+
+    public static void displayIndexedList(ArrayList<String> list) {
+        System.out.println("List is: " + list);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(i + ": " + list.get(i));
+        }
     }
 }
